7a6e09a313894bc99b94191c735dde367b56ea2d
Slava Makeev <unclearnuclear@yahoo.com>
2023-07-03 22:07:43 +0000

    x

--- api/capital-finder.py
+++ api/capital-finder.py
@@ -1,3 +1,6 @@
+"""
+Python files within the api directory, containing an handler variable that inherits from the BaseHTTPRequestHandler
+"""
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import requests
@@ -11,23 +14,29 @@ class handler(BaseHTTPRequestHandler):
         query_string_list = parse.parse_qsl(url_components.query)
         dic = dict(query_string_list)
 
-        # retrieve the capital from the query parameters
-        capital = dic.get("capital", "")
-
-        # create the URL with the capital name
-        url = f"https://restcountries.com/v3.1/capital/{capital}"
-
-        # send a GET request to the Restcountries API
-        response = requests.get(url)
-        data = response.json()
-
         # response code
         self.send_response(200)
         # headers
         self.send_header("Content-type", "text/plain")
         self.end_headers()
 
-        # write the response body
-        self.wfile.write(str(data).encode())
+        capital = dic.get("capital", "")
+
+        # create message
+        url = f"https://restcountries.com/v3.1/capital/{capital}"
+
+        response = requests.get(url)
+        data = response.json()
+        definitions = []
+        for country_data in data:
+            try:
+                definition = country_data["meanings"][0]["definitions"][0]["definition"]
+                definitions.append(definition)
+            except (KeyError, IndexError):
+                pass
+
+        message = str(definitions)
+
+        self.wfile.write(message.encode())
 
         return
